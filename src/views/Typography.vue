ab24c049ec5b2e39f3ebcb5163ec0756474febc2
sangwon <sangwon@sangwonui-iMac.local>
2021-12-07 15:29:05 +0900

    typography

--- src/views/Typography.vue
+++ src/views/Typography.vue
@@ -27,6 +27,14 @@
                     <p class="font-weight-thin">
                         font-weight-thin
                     </p>
+                    <p class="text-justify">
+                        Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.
+
+                        Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.
+                    </p>
+                    <p class="text-center text-sm-right">
+                        Left aligned text on all viewport sizes.
+                    </p>
                     Heading 4
                     Heading 5
                     Heading 6
