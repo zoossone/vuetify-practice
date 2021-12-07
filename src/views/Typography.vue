be1940d4582d16e9d09110b39a9120881d0cd8e4
sangwon <sangwon@sangwonui-iMac.local>
2021-12-07 15:40:50 +0900

    color

--- src/views/Typography.vue
+++ src/views/Typography.vue
@@ -4,50 +4,55 @@
             <v-card-title>
                 Typography
             </v-card-title>
-            <v-container class="grey lighten-3">
-                <v-card
-                    class="pa-3"
-                    outlined
-                >
-                    <p class="text-h1">
-                        Heading 1
-                    </p>
-                    <p class="text-h2">
-                        Heading 2
-                    </p>
-                    <p class="text-h3 text-sm-subtitle-1">
-                        Heading 3
-                    </p>
-                    <p class="display-4">
-                        display-4
-                    </p>
-                    <p class="font-weight-black">
-                        Black text
-                    </p>
-                    <p class="font-weight-thin">
-                        font-weight-thin
-                    </p>
-                    <p class="text-justify">
-                        Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.
-
-                        Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.
-                    </p>
-                    <p class="text-center text-sm-right">
-                        Left aligned text on all viewport sizes.
-                    </p>
-                    Heading 4
-                    Heading 5
-                    Heading 6
-                    Subtitle 1
-                    Subtitle 2
-                    Body 1
-                    Body 2
-                    BUTTON
-                    Caption
-                    OVERLINE
-
+            <v-card-text>
+                <!-- 프라이머리 색상등 기존에 가지고 있는 지정 색상을 변경하고 싶으면 vuetify.js 파일로 가서 vuetify.js 사이트에 있는 preset의 속성 값을 재정의하면 된다
+                또한 vuetify.js 사이트에서 color를 검색하면 사용할 수 있는 다양한 팔레트가 존재-->
+                <v-card class="pa-3 text-h2 primary lighten-1">
+                    컨텐츠
                 </v-card>
-            </v-container>
+                <v-card class="pa-3 text-h2 secondary">
+                    컨텐츠
+                </v-card>
+                <v-card class="pa-3 text-h2 error">
+                    컨텐츠
+                </v-card>
+                <v-card class="pa-3 text-h2 primary--text">
+                    컨텐츠
+                </v-card>
+                <v-card class="pa-3 text-h2 secondary--text">
+                    컨텐츠
+                </v-card>
+                <v-card class="pa-3 text-h2 error--text">
+                    컨텐츠
+                </v-card>
+                <p class="text-h1">
+                    Heading 1
+                </p>
+                <p class="text-h2">
+                    Heading 2
+                </p>
+                <p class="text-h3 text-sm-subtitle-1">
+                    Heading 3
+                </p>
+                <p class="display-4">
+                    display-4
+                </p>
+                <p class="font-weight-black">
+                    Black text
+                </p>
+                <p class="font-weight-thin">
+                    font-weight-thin
+                </p>
+                <p class="text-justify">
+                    Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.
+
+                    Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.
+                </p>
+                <p class="text-center text-sm-right">
+                    Left aligned text on all viewport sizes.
+                </p>
+            </v-card-text>
+
         </v-card>
     </v-container>
 </template>
