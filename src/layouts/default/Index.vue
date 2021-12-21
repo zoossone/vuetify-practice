08fec1849bcf10b7fd608572b4d9a8451909065f
sangwon <sangwon@sangwonui-iMac.local>
2021-12-21 16:41:03 +0900

    layouts

---
+++ src/layouts/default/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
+      </div>
+
+      <v-spacer />
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      app
+      :src="require('@/assets/sidebar.jpg')"
+    >
+      <!-- #img === v-slot:img -->
+      <template
+        #img="props"
+      >
+        <v-img
+          :gradient="gredient"
+          v-bind="props"
+        />
+      </template>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-spacer />
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+          active-class="primary"
+          class="py-1"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'DefaultLayout',
+    data: () => ({
+      drawer: false,
+      gredient: 'rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)',
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
+          { title: 'Grid System', icon: 'mdi-view-dashboard', to: '/grid-system' },
+          { title: 'Grid List Page', icon: 'mdi-view-dashboard', to: '/grid-list-page' },
+          { title: 'Breakpoints', icon: 'mdi-view-dashboard', to: '/breakpoints' },
+          { title: 'Typography', icon: 'mdi-view-dashboard', to: '/typography' },
+          { title: 'Tables', icon: 'mdi-view-dashboard', to: '/tables' },
+          { title: 'Forms', icon: 'mdi-view-dashboard', to: '/forms' },
+          { title: 'Buttons', icon: 'mdi-view-dashboard', to: '/buttons' },
+          { title: 'Icons', icon: 'mdi-view-dashboard', to: '/icons' },
+          { title: 'SignIn', icon: 'mdi-login', to: '/authentication/sign-in' },
+          { title: 'SignUp', icon: 'mdi-logout', to: '/authentication/sign-up' },
+          { title: 'ProductList', icon: 'mdi-reproduction', to: '/page/product-list' },
+        ],
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,54 +9,93 @@ import Tables from '@/views/Tables';
 import Forms from '@/views/Forms';
 import Buttons from '@/views/Buttons';
 import Icons from '@/views/Icons';
+import SignIn from '@/views/authentication/SignIn';
+import SignUp from '@/views/authentication/SignUp';
+import ProductList from '@/views/page/ProductList';
+import DefaultLayout from '@/layouts/default/Index';
+import PageLayout from '@/layouts/page/Index';
+import AuthenticationLayout from '@/layouts/authentication/Index';
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
+        component: DefaultLayout,
+        children: [
+          {
+              path: '/',
+              name: 'Dashboard',
+              component: Dashboard
+          },
+          {
+              path: '/grid-system',
+              name: 'GridSystem',
+              component: GridSystem
+          },
+          {
+              path: '/grid-list-page',
+              name: 'GridList',
+              component: GridListPage
+          },
+          {
+              path: '/breakpoints',
+              name: 'Breakpoints',
+              component: Breakpoints
+          },
+          {
+              path: '/typography',
+              name: 'Typography',
+              component: Typography
+          },
+          {
+              path: '/tables',
+              name: 'Tables',
+              component: Tables
+          },
+          {
+              path: '/forms',
+              name: 'Forms',
+              component: Forms
+          },
+          {
+              path: '/buttons',
+              name: 'Buttons',
+              component: Buttons
+          },
+          {
+              path: '/icons',
+              name: 'Icons',
+              component: Icons
+          },
+        ]
     },
     {
-        path: '/grid-system',
-        name: 'GridSystem',
-        component: GridSystem
+        path: '/authentication',
+        component: AuthenticationLayout,
+        children: [
+          {
+              path: 'sign-in',
+              name: 'SignIn',
+              component: SignIn
+          },
+          {
+              path: 'sign-up',
+              name: 'SignUp',
+              component: SignUp
+          },
+        ]
     },
     {
-        path: '/grid-list-page',
-        name: 'GridList',
-        component: GridListPage
-    },
-    {
-        path: '/breakpoints',
-        name: 'Breakpoints',
-        component: Breakpoints
-    },
-    {
-        path: '/typography',
-        name: 'Typography',
-        component: Typography
-    },
-    {
-        path: '/tables',
-        name: 'Tables',
-        component: Tables
-    },
-    {
-        path: '/forms',
-        name: 'Forms',
-        component: Forms
-    },
-    {
-        path: '/buttons',
-        name: 'Buttons',
-        component: Buttons
-    },
-    {
-        path: '/icons',
-        name: 'Icons',
-        component: Icons
+        path: '/page',
+        component: PageLayout,
+        children: [
+          {
+              path: 'product-list',
+              name: 'ProductList',
+              component: ProductList
+          },
+        ]
     },
 ];
 
