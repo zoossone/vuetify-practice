6a7d17f06f8120b1552bb54cf0ce8c7c771378ce
sangwon <sangwon@sangwonui-iMac.local>
2021-12-22 17:48:21 +0900

    vee-validate

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,105 +1,85 @@
 <template>
   <v-container>
-    <v-card class="pa-3">
-      <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-      >
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
-          required
-        />
-
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="E-mail"
-          required
-        />
-
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[v => !!v || 'Item is required']"
-          label="Item"
-          required
-        />
-
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[v => !!v || 'You must agree to continue!']"
-          label="Do you agree?"
-          required
-        />
-
-        <v-btn
-          :disabled="!valid"
-          color="success"
-          class="mr-4"
-          @click="validate"
-        >
-          Validate
-        </v-btn>
-
-        <v-btn
-          color="error"
-          class="mr-4"
-          @click="reset"
-        >
-          Reset Form
-        </v-btn>
-
-        <v-btn
-          color="warning"
-          @click="resetValidation"
-        >
-          Reset Validation
-        </v-btn>
-      </v-form>
+    <v-card>
+      <v-card-title>
+        Form Validation
+      </v-card-title>
+      <v-card-text>
+        <validation-observer>
+          <v-form>
+            <validation-provider
+              v-slot="{ errors }"
+              name="Name"
+              rules="max:10"
+            >
+              <v-text-field
+                v-model="name"
+                label="Name"
+                :counter="10"
+                :error-messages="errors"
+              />
+            </validation-provider>
+            <validation-provider>
+              <v-text-field
+                label="PhoneNumber"
+              />
+            </validation-provider>
+            <validation-provider>
+              <v-text-field
+                label="E-mail"
+              />
+            </validation-provider>
+            <validation-provider>
+              <v-select
+                label="select"
+                :items="item"
+              />
+            </validation-provider>
+            <validation-provider>
+              <v-checkbox
+                label="Checkbox"
+                value="1"
+              />
+            </validation-provider>
+            <v-btn
+              class="mr-4"
+              type="submit"
+              color="primary"
+            >
+              submit
+            </v-btn>
+            <v-btn>
+              clear
+            </v-btn>
+          </v-form>
+        </validation-observer>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            valid: true,
-            name: '',
-            nameRules: [
-                v => !!v || 'Name is required',
-                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-            ],
-            email: '',
-            emailRules: [
-                v => !!v || 'E-mail is required',
-                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-            ],
-            select: null,
-            items: [
-                'Item 1',
-                'Item 2',
-                'Item 3',
-                'Item 4',
-            ],
-            checkbox: false,
-            }),
-
-        methods: {
-            validate () {
-                this.$refs.form.validate()
-            },
-            reset () {
-                this.$refs.form.reset()
-            },
-            resetValidation () {
-                this.$refs.form.resetValidation()
-            },
-        },
-    }
+import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
+import { max } from 'vee-validate/dist/rules';
+extend('max', {
+  ...max,
+  message: '{_field_}필드는 {length}자를 초과할 수 없습니다.'
+});
+export default {
+  name: 'Forms',
+  components: {
+    ValidationObserver,
+    ValidationProvider
+  },
+  data: () => ({
+    name: null,
+    item : [
+      { text: '아이템1', value: 1},
+      { text: '아이템2', value: 2},
+      { text: '아이템3', value: 3},
+    ],
+  })
+}
 </script>
 
 <style scoped>
