855fc41ddaafd08681f389ed72e782f569700855
sangwon <sangwon@sangwonui-iMac.local>
2021-12-22 18:30:50 +0900

    vee_validation_2

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -5,12 +5,15 @@
         Form Validation
       </v-card-title>
       <v-card-text>
-        <validation-observer>
-          <v-form>
+        <validation-observer
+          ref="observer"
+          v-slot="{ invalid }"
+        >
+          <v-form @submit.prevent="submit">
             <validation-provider
               v-slot="{ errors }"
               name="Name"
-              rules="max:10"
+              rules="required|max:10"
             >
               <v-text-field
                 v-model="name"
@@ -19,32 +22,66 @@
                 :error-messages="errors"
               />
             </validation-provider>
-            <validation-provider>
+            <validation-provider
+              v-slot="{ errors }"
+              name="PhoneNumber"
+              :rules="{
+                required: true,
+                numeric: true,
+                digits: 11
+              }"
+            >
               <v-text-field
+                v-model="phoneNumber"
                 label="PhoneNumber"
+                :counter="11"
+                :error-messages="errors"
               />
             </validation-provider>
-            <validation-provider>
+            <validation-provider
+              v-slot="{ errors }"
+              name="E-Mail"
+              :rules="{
+                required: true,
+                email: true
+              }"
+            >
               <v-text-field
-                label="E-mail"
+                v-model="email"
+                label="E-Mail"
+                :error-messages="errors"
               />
             </validation-provider>
-            <validation-provider>
+            <validation-provider
+              v-slot="{ errors }"
+              name="Select"
+              :rules="{ required: true }"
+            >
               <v-select
-                label="select"
+                v-model="select"
+                label="Select"
                 :items="item"
+                :error-messages="errors"
               />
             </validation-provider>
-            <validation-provider>
+            <validation-provider
+              v-slot="{ errors }"
+              name="Checkbox"
+              :rules="{ required: true }"
+              :error-messages="errors"
+            >
               <v-checkbox
+                v-model="checkbox"
                 label="Checkbox"
                 value="1"
+                :error-messages="errors"
               />
             </validation-provider>
             <v-btn
               class="mr-4"
               type="submit"
               color="primary"
+              :disabled="invalid"
             >
               submit
             </v-btn>
@@ -60,11 +97,33 @@
 
 <script>
 import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
-import { max } from 'vee-validate/dist/rules';
+import {
+  max,
+  required,
+  numeric,
+  digits,
+  email
+ } from 'vee-validate/dist/rules';
 extend('max', {
   ...max,
-  message: '{_field_}필드는 {length}자를 초과할 수 없습니다.'
+  message: '{_field_} 필드는 {length}자를 초과할 수 없습니다.'
 });
+extend('required', {
+  ...required,
+  message: '{_field_} 필드는 필수값 입니다.'
+});
+extend('numeric', {
+  ...numeric,
+  message: '{_field_} 필드는 숫자로만 구성되어야 합니다.'
+});
+extend('digits', {
+  ...digits,
+  message: '{_field_} 필드는 {length} 자리여야합니다.'
+});
+extend('email', {
+  ...email,
+  message: '잘못 입력된 이메일 주소입니다.'
+} )
 export default {
   name: 'Forms',
   components: {
@@ -73,12 +132,34 @@ export default {
   },
   data: () => ({
     name: null,
+    phoneNumber: null,
+    email: null,
+    select: null,
+    checkbox: null,
+    errors: null,
     item : [
       { text: '아이템1', value: 1},
       { text: '아이템2', value: 2},
       { text: '아이템3', value: 3},
     ],
-  })
+  }),
+  methods: {
+    submit() {
+      this.$refs.observer.validate().then(result => {
+        console.log('result: ',result);
+        if(result) {
+          alert('양식 제출')
+        }
+      })
+    },
+    clear() {
+      this.name = null,
+      this.phoneNumber = null,
+      this.email = null,
+      this.select = null,
+      this.checkbox = null
+    }
+  }
 }
 </script>
 
