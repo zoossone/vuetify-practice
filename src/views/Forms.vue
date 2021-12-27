3e988ce93796230726b6f739a66c5ec2a09d0900
sangwon <sangwon@sangwonui-iMac.local>
2021-12-27 11:48:52 +0900

    validation_form

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -96,40 +96,9 @@
 </template>
 
 <script>
-import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
-import {
-  max,
-  required,
-  numeric,
-  digits,
-  email
- } from 'vee-validate/dist/rules';
-extend('max', {
-  ...max,
-  message: '{_field_} 필드는 {length}자를 초과할 수 없습니다.'
-});
-extend('required', {
-  ...required,
-  message: '{_field_} 필드는 필수값 입니다.'
-});
-extend('numeric', {
-  ...numeric,
-  message: '{_field_} 필드는 숫자로만 구성되어야 합니다.'
-});
-extend('digits', {
-  ...digits,
-  message: '{_field_} 필드는 {length} 자리여야합니다.'
-});
-extend('email', {
-  ...email,
-  message: '잘못 입력된 이메일 주소입니다.'
-} )
+
 export default {
   name: 'Forms',
-  components: {
-    ValidationObserver,
-    ValidationProvider
-  },
   data: () => ({
     name: null,
     phoneNumber: null,
--- src/views/authentication/SignUp.vue
+++ src/views/authentication/SignUp.vue
@@ -2,37 +2,77 @@
   <v-container class="fill-height">
     <v-row justify="center">
       <v-col cols="auto">
+        <router-link to="/">
+          <v-img
+            class="mx-auto mb-6"
+            width="140"
+            :src="require('@/assets/logo.png')"
+          />
+        </router-link>
         <v-card
           width="460"
         >
           <v-card-text class="text-center px-12 py-16">
-            <div class="text-h4 font-weight-black mb-10">
-              회원가입
-            </div>
-            <v-text-field
-              label="이메일"
-              clearable
-              prepend-icon="mdi-email"
-            />
-            <v-text-field
-              label="비밀번호"
-              clearable
-              prepend-icon="mdi-lock-outline"
-            />
-            <v-text-field
-              label="비밀번호 확인"
-              clearable
-              prepend-icon="mdi-lock-outline"
-            />
-            <v-btn
-              class="mt-6"
-              block
-              x-large
-              rounded
-              color="primary"
+            <validation-observer
+              ref="observer"
+              v-slot="{ invalid }"
             >
-              가입하기
-            </v-btn>
+              <v-form @submit.prevent="signUp">
+                <div class="text-h4 font-weight-black mb-10">
+                  회원가입
+                </div>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="이메일"
+                  :rules="{ required: true }"
+                >
+                  <v-text-field
+                    v-model="email"
+                    label="이메일"
+                    clearable
+                    prepend-icon="mdi-email"
+                    :error-messages="errors"
+                  />
+                </validation-provider>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="비밀번호"
+                  :rules="{ required: true }"
+                >
+                  <v-text-field
+                    v-model="password"
+                    label="비밀번호"
+                    clearable
+                    prepend-icon="mdi-lock-outline"
+                    :error-messages="errors"
+                  />
+                </validation-provider>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="비밀번호 확인"
+                  :rules="{ required: true, confirmed: '비밀번호' }"
+                >
+                  <v-text-field
+                    v-model="passwordConfirm"
+                    label="비밀번호 확인"
+                    clearable
+                    prepend-icon="mdi-lock-outline"
+                    :error-messages="errors"
+                  />
+                </validation-provider>
+                <v-btn
+                  class="mt-6"
+                  type="submit"
+                  block
+                  x-large
+                  rounded
+                  color="primary"
+                  :disabled="invalid"
+                >
+                  가입하기
+                </v-btn>
+              </v-form>
+            </validation-observer>
           </v-card-text>
         </v-card>
       </v-col>
@@ -42,7 +82,20 @@
 
 <script>
   export default {
-
+    name: 'SignUp',
+    data: () => ({
+      email: null,
+      password: null,
+      passwordConfirm: null
+    }),
+    methods: {
+      async signUp () {
+        const result = await this.$refs.observer.validate();
+        if (result) {
+          alert('회원가입 프로세스~');
+        }
+      }
+    }
   }
 </script>
 
